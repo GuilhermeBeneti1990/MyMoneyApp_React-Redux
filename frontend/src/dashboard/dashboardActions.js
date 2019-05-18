e4577ddf0b120ef88496fcc87cab9ccb6cab4126
Guilherme Beneti <guilhermebeneti@MacBook-Pro-de-Guilherme.local>
2019-05-19 00:36:10 +0100

    Dashboard error fixed

--- frontend/src/dashboard/dashboardActions.js
+++ frontend/src/dashboard/dashboardActions.js
@@ -5,7 +5,7 @@ const BASE_URL = 'http://localhost:3003/api'
 export function getSummary() {
     const request = axios.get(`${BASE_URL}/billingCycles/summary`)
     return {
-        type: 'BILLING_SUMARY_FETCHED',
+        type: 'BILLING_SUMMARY_FETCHED',
         payload: request
     }
 }
